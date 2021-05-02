509c1ccf82efad069b1f1ef65f5c924f3e5f681f
Jangseongjin <sungjin5891@gmail.com>
2021-05-02 17:02:47 +0900

    Create SpringBoot With Kotlin

---
+++ src/test/kotlin/com/example/springexample2/SpringExample2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springexample2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringExample2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
