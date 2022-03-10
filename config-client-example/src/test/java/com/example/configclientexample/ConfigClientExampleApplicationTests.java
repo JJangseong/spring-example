ed26c5deca99b3f04aba020d2f9ed3cfeeb27720
sungjin96 <sungjin5891@gmail.com>
2022-03-10 19:52:13 +0900

    spring-config

---
+++ config-client-example/src/test/java/com/example/configclientexample/ConfigClientExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configclientexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
